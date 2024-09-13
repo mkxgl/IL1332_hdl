e35dd67a1ac6b116df4b1bce2772ff5e69423bc1
Marty K Gludkowska <mkgldkwsk@gmail.com>
2024-09-13 19:04:56 +0200

    Update CLA_carry_0.sv
    Fix nand2_delay naming convention

--- CLA_8-bit/CLA_carry_0.sv
+++ CLA_8-bit/CLA_carry_0.sv
@@ -2,33 +2,32 @@ module CLA_carry_logic_0
 (
   input logic p0,
   input logic g0,
-  input logic carry_in0,
-  output logic carry_out0
+  input logic carry_in_0,
+  output logic carry_out
 );
+	logic p0_nand_carry_in_0;  
+  logic g0_inv;  
 
-    logic p0_nand_carry_in0;  
-    logic g0_inv;  
 
-
-  nand_2 u1 
-  (
+  nand2_delay u1 
+	(
 	  .a(p0), 
-	  .b(ci0), 
-	  .y(p0_nand_carry_in0)
+	  .b(carry_in_0), 
+	  .y(p0_nand_carry_in_0)
 	); 
 
-  nand_2 u2 
+  nand2_delay u2 
   (
 	  .a(g0), 
 	  .b(g0), 
     .y(g0_inv)
 	); 
 
-  nand_2 u3  
+  nand2_delay u3  
   (
-	  .a(p0_nand_carry_in0), 
+	  .a(p0_nand_carry_in_0), 
     .b(g0_inv), 
-    .y(carry_out0)
+		.y(carry_out)
 	); 
 
 endmodule
