7e8d596f55f8649caa5182a0410a7fe84711dd19
Marty K Gludkowska <mkgldkwsk@gmail.com>
2024-10-14 15:25:29 +0200

    Update logic_module.sv

--- ALU/logic/logic_module.sv
+++ ALU/logic/logic_module.sv
@@ -1,30 +1,29 @@
 module logic_unit
 (
-	input logic [7:0] x,
-	input logic [7:0] y,
-	// no m signal since its all 11 for all logical operations 
+	input logic [7:0] X,
+	input logic [7:0] Y,
 	input logic [1:0] s,
-	output logic [15:0] z
+	output logic [15:0] Z
 );
 
 		// LOGIC OPERATIONS //
 	logic [7:0] and_result;
 	logic [7:0] nand_result;
 	logic [7:0] or_result;
-	logic [7:0] not_x;
-	logic [7:0] not_x_result;
-	logic [7:0] not_y;
-	logic [7:0] not_y_result;
+	logic [7:0] not_X;
+	logic [7:0] not_X_result;
+	logic [7:0] not_Y;
+	logic [7:0] not_Y_result;
 	logic [7:0] xor_result;
 		
 	genvar i;
 	// AND
 	generate
 		for (i = 0; i < 8; ++i) begin: and_operation
-			nand2_delay nand_x_y
+			nand2_delay nand_X_Y
 			(
-				.a(x[i]),
-				.b(y[i]),
+				.a(X[i]),
+				.b(Y[i]),
 				.y(nand_result[i])
 			);
 
@@ -40,24 +39,24 @@ module logic_unit
 	// OR
 	generate
 		for (i = 0; i < 8; ++i) begin: or_operation
-			nand2_delay not_x2
+			nand2_delay not_X2
 			(
-				.a(x[i]),
+				.a(X[i]),
 				.b(x[i]),
-				.y(not_x_result[i])
+				.y(not_X_result[i])
 			);
 
-			nand2_delay not_y2
+			nand2_delay not_Y2
 			(
-				.a(y[i]),
-				.b(y[i]),
-				.y(not_y_result[i])
+				.a(Y[i]),
+				.b(Y[i]),
+				.y(not_Y_result[i])
 			);
 
-			nand2_delay nand_notx_noty
+			nand2_delay nand_notX_noty
 			(
-				.a(not_x_result[i]),
-				.b(not_y_result[i]),
+				.a(not_X_result[i]),
+				.b(not_Y_result[i]),
 				.y(or_result[i])
 			);
 		end
@@ -65,24 +64,24 @@ module logic_unit
 
 	// NOT X
 	generate
-		for (i = 0; i < 8; ++i) begin: not_x_operation
-			nand2_delay not_x
+		for (i = 0; i < 8; ++i) begin: not_X_operation
+			nand2_delay not_X
 			(
-				.a(x[i]),
-				.b(x[i]),
-				.y(not_x[i])
+				.a(X[i]),
+				.b(X[i]),
+				.y(not_X[i])
 			);
 		end
 	endgenerate	
 
 	// NOT Y
 	generate
-		for (i = 0; i < 8; ++i) begin: not_y_operation
-			nand2_delay not_y
+		for (i = 0; i < 8; ++i) begin: not_Y_operation
+			nand2_delay not_Y
 			(
-				.a(y[i]),
-				.b(y[i]),
-				.y(not_y[i])
+				.a(Y[i]),
+				.b(Y[i]),
+				.y(not_Y[i])
 			);
 		end
 	endgenerate
@@ -92,8 +91,8 @@ module logic_unit
 		for (i = 0; i < 8; ++i) begin: xor_operation
 	        	xor2_delay xor_gate
 			(
-				.a(x[i]), 
-				.b(y[i]), 
+				.a(X[i]), 
+				.b(Y[i]), 
 				.y(xor_result[i])
 			);
 	        end
@@ -101,9 +100,9 @@ module logic_unit
 		
 	always @(*) begin
 		case (s) // we sign extend these bad boys with {8{}
-			2'b00: z = {{8{and_result[7]}}, and_result};
+		2'b00: z = {{8{and_result[7]}}, and_result};
 	        2'b01: z = {{8{or_result[7]}}, or_result};
-	        2'b10: z = {not_x, not_y};	// Concatenated NOT x with NOT y
+		2'b10: z = {not_X, not_Y};	// Concatenated NOT x with NOT y
 	        2'b11: z = {{8{xor_result[7]}}, xor_result};
 		default : z = 16'b0;
 		endcase
