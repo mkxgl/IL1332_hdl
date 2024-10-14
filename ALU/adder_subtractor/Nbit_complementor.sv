20ffe0ee77923ae9e9fdb4615e2ff0f8f04c4d40
Marty K Gludkowska <mkgldkwsk@gmail.com>
2024-10-14 17:54:43 +0200

    Update Nbit_complementor.sv

--- ALU/adder_subtractor/Nbit_complementor.sv
+++ ALU/adder_subtractor/Nbit_complementor.sv
@@ -2,19 +2,23 @@
 module Nbit_complementor #(parameter N=8)
 (
 	input [N-1:0] B, // 8 bit number lets say 0010 1100
-	input cin,
+	input cin, // 0?
 	output [N-1:0] Y // 2s complement, should be 1101 0100
 );
-
+	logic [7:0] result;
+	
 	genvar i;
 	generate
 		for(i = 0; i < N; i++) begin : xor_slice 
-				xor2_delay xn(
-					.a(cin),
-					.b(B[i]),
-					.y(Y[i])
-				);
+			xor2_delay xn
+			(
+				.a(cin), // 1
+				.b(B[i]), // 0010 1100
+				.y(result[i]) // 1101 0011
+			);
 			end
 		endgenerate
+
+	assign Y = result + 1;
 
 endmodule
