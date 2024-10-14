7063378a494e2682b0b68b11c8ce90e08f063c44
Marty K Gludkowska <mkgldkwsk@gmail.com>
2024-10-14 17:59:20 +0200

    Update Nbit_complementor.sv

--- ALU/adder_subtractor/Nbit_complementor.sv
+++ ALU/adder_subtractor/Nbit_complementor.sv
@@ -3,9 +3,8 @@ module Nbit_complementor #(parameter N=8)
 (
 	input [N-1:0] B, // 8 bit number lets say 0010 1100
 	input cin, // 0?
-	output [N-1:0] Y // 2s complement, should be 1101 0100
+	output [N-1:0] Y
 );
-	logic [7:0] result;
 	
 	genvar i;
 	generate
@@ -14,11 +13,9 @@ module Nbit_complementor #(parameter N=8)
 			(
 				.a(cin), // 1
 				.b(B[i]), // 0010 1100
-				.y(result[i]) // 1101 0011
+				.y(Y[i]) // 1101 0011
 			);
 			end
 		endgenerate
-
-	assign Y = result + 1;
 
 endmodule
