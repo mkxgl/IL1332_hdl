a39da6d567f1165ddd811668f189d6df5833d170
Marty K Gludkowska <mkgldkwsk@gmail.com>
2024-10-14 15:26:51 +0200

    Update alu.sv

--- ALU/alu.sv
+++ ALU/alu.sv
@@ -1,10 +1,10 @@
 module alu
 (
-    input logic [7:0] x,
-    input logic [7:0] y,
+    input logic [7:0] X,
+    input logic [7:0] Y,
     input logic [1:0] m,
     input logic [1:0] s,
-    output logic [15:0] z
+    output logic [15:0] Z
 );
     
     logic [15:0] arithmetic_result;
@@ -14,42 +14,42 @@ module alu
 
     arithmetic_unit arithmetic 
     (
-        .x(x),
-        .y(y),
+        .X(x),
+        .Y(y),
         .m(m),
-        .z(arithmetic_result)
+        .Z(arithmetic_result)
     );
 
     shifter_rotator_unit shifter 
     (
-        .x(x),
-        .y(y),
+        .X(x),
+        .Y(y),
         .m(m),
-        .z(shifter_result)
+        .Z(shifter_result)
     );
 
     comparator_unit comparator 
     (
-        .x(x),
-        .y(y),
+        .X(x),
+        .Y(y),
         .m(m),
-        .z(comparator_result)
+        .Z(comparator_result)
     );
 
     logic_unit logic_op 
     (
-        .x(x),
-        .y(y),
+        .X(x),
+        .Y(y),
         .m(m),
-        .z(logic_result)
+        .Z(logic_result)
     );
 
     always_comb begin
         case (s)
-            2'b00: z = arithmetic_result;
-            2'b01: z = shifter_result;
-            2'b10: z = comparator_result;
-            2'b11: z = logic_result;
+            2'b00: Z = arithmetic_result;
+            2'b01: Z = shifter_result;
+            2'b10: Z = comparator_result;
+            2'b11: Z = logic_result;
             default: z = 16'b0;
         endcase
     end
